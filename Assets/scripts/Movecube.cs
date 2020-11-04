8de81240f6443d549bf2d9293e74fc3dc909a59d
SumanaiEugen <ryanlim943@gmail.com>
2020-11-04 16:17:34 +0800

    MiniCode2

---
+++ Assets/scripts/Movecube.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movecube : MonoBehaviour
+{
+    float forwardSpeed = 5f;
+    float zlimit = 27.5f;
+    float zlimit2 = 6f;
+    bool hitwall = false;
+    public GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (transform.position.z < zlimit && hitwall == false)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * forwardSpeed);
+        }
+        if (transform.position.z > zlimit)
+        {
+            hitwall = true;
+        }
+        if (hitwall == true)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * -forwardSpeed);
+        }
+        if (transform.position.z < zlimit2)
+        {
+            hitwall = false;
+        }
+
+    }
+    //parent for the player
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            Player.transform.parent = transform;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            Player.transform.parent = null;
+        }
+    }
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float speed = 10f;
+    float gravityModifier = 3f;
+    bool isgrounded;
+    Rigidbody playerRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float VerticalInput = Input.GetAxis("Vertical");
+        //float HorizontalInput = Input.GetAxis("Horziontal");
+
+        //movement of player
+        transform.Translate(Vector3.forward * Time.deltaTime * VerticalInput * speed);
+        //transform.Translate(Vector3.right * Time.deltaTime * HorizontalInput * speed);
+        
+        Jumpplayer();
+    }
+    void Jumpplayer()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isgrounded == true)
+        {
+            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            isgrounded = false;
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("StartCube"))
+        {
+            isgrounded = true;
+        }
+        if (collision.gameObject.CompareTag("MoveCube"))
+        {
+            isgrounded = true;
+        }
+    }
+
+}
